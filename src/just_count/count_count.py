2416f769472452f72d3cc53a44580d95cb41781f
AdamE-Nobot <adam.ebrahim.real@gmail.com>
2024-11-18 14:09:09 +0100

    Niks veranderd. alleen enters (ging automatisch)

--- src/just_count/count_count.py
+++ src/just_count/count_count.py
@@ -1,11 +1,13 @@
-import just_count.square
 import click
+
+import just_count.square
+
 
 @click.command()
 @click.argument("number", type=float)
 def main(number):
     print(f"The square of {number} is {just_count.square.square(number)}")
 
-if __name__ == '__main__':
-    main()
 
+if __name__ == "__main__":
+    main()
